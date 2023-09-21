62d018646adc5c2ff2fa89cf81038a46a1aafacf
aldinalj <aldinalj@gmail.com>
2023-09-21 12:22:21 +0200

    change in magma

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+        System.out.println("Change 2");
     }
 }
